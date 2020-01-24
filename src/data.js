80b55dff5c6be5d236c060fefa353a4cce3f0f5d
Caroline Chen <carolinecaroline@gmail.com>
2020-01-24 11:37:50 -0500

    create seed data

---
+++ src/data.js
@@ -0,0 +1,51 @@
+const items = {
+	item1: {
+		date: "1/1/2020",
+		itemName: "Publication 1",
+		indexPage: 12345,
+		developer: "Bob",
+		flag: "standard"
+	},
+	item2: {
+		date: "2/2/2020",
+		itemName: "Publication 2",
+		indexPage: 23456,
+		developer: "Linda",
+		flag: "priority"
+	},
+	item3: {
+		date: "3/3/2020",
+		itemName: "Publication 3",
+		indexPage: 34567,
+		developer: "Tina",
+		flag: "standard"
+	},
+	item4: {
+		date: "4/4/2020",
+		itemName: "Publication 4",
+		indexPage: 45678,
+		developer: "Gene",
+		flag: "standard"
+	},
+	item5: {
+		date: "5/5/2020",
+		itemName: "Publication 5",
+		indexPage: 56789,
+		developer: "Louise",
+		flag: "same-day"
+	},
+	item6: {
+		date: "6/6/2020",
+		itemName: "Publication 6",
+		indexPage: 67890,
+		developer: "Teddy",
+		flag: "priority"
+	},
+	item7: {
+		date: "7/7/2020",
+		itemName: "Publication 7",
+		indexPage: 78901,
+		developer: "Gayle",
+		flag: "standard"
+	}
+}
